01200ba861c9434cba6527375f9d9de378b17926
Filming <79931565+filming@users.noreply.github.com>
2023-05-17 18:52:57 -0400

    Add get_player_data method to Clash_Studio class

--- src/clashstudio/__init__.py
+++ src/clashstudio/__init__.py
@@ -1,10 +1,17 @@
-
-
-
+import requests
+import json
+import urllib
 
 class Clash_Studio():
     def __init__(self, API_TOKEN):
         self.headers = {"content-type":"application/json", "Authorization":f"Bearer {API_TOKEN}"}
+    
+    def get_player_data(self, player_tag):
+        safe_tag = urllib.parse.quote_plus(player_tag)
 
+        r = requests.get(f"https://api.clashofclans.com/v1/players/{safe_tag}", headers = self.headers)
+        resp = json.loads(r.text)
+
+        return resp
 
     
