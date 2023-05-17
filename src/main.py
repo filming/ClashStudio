1a35d424525cb0273a2dafae0c2155d2f5c75b96
Filming <79931565+filming@users.noreply.github.com>
2023-05-17 18:53:35 -0400

    Add general usage code to main file

--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ from clashstudio import Clash_Studio
 
 load_dotenv()
 
-
 def main():
     COC_API_TOKEN = os.getenv("COC_API_TOKEN")
+    main_acc_tag = os.getenv("MAIN_ACCOUNT_TAG")
+    second_acc_tag = os.getenv("SECOND_ACCOUNT_TAG")
+    donation_acc_tag = os.getenv("DONATION_ACCOUNT_TAG")
 
     cs = Clash_Studio(COC_API_TOKEN)
 
-
-
-
+    cs.get_player_data(main_acc_tag)
 
 if __name__ == "__main__":
     main()
